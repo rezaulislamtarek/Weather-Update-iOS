63b38daadf0fc6920e280d98d11eee62bc17bcde
rezaulislam <mrezaulislam.t@gmail.com>
2023-06-20 22:55:10 +0600

    Initial Commit

---
+++ Weather Update/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather Update
+//
+//  Created by Rezaul Islam on 20/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather Update/Weather_UpdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_UpdateApp.swift
+//  Weather Update
+//
+//  Created by Rezaul Islam on 20/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_UpdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
