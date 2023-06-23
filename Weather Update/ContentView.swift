e3461d0e13a86de932a555ebea5cdd637e8db507
rezaulislam <mrezaulislam.t@gmail.com>
2023-06-23 09:31:34 +0600

    refactor to seperate view

--- Weather Update/ContentView.swift
+++ Weather Update/ContentView.swift
@@ -8,28 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+     
     var body: some View {
         ZStack{
-            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing  )
-                .edgesIgnoringSafeArea(.all)
+            BackgroundView(isNight: true)
             
             VStack {
-                Text("Dhaka, BD")
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding()
+                CityView()
                 
-                VStack(spacing:10){
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    Text("76°")
-                        .font(.system(size: 50, weight: .medium))
-                        .foregroundColor(.white)
-                }.padding(.bottom,40)
+                MainWeatherView()
                 
                 HStack(spacing:20){
                     WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 73)
@@ -45,13 +32,7 @@ struct ContentView: View {
                 Button{
                     
                 }label: {
-                    Text("Change Day Time")
-                        .foregroundColor(.blue)
-                        .fontWeight(.bold)
-                        .font(.title3)
-                        .frame(width: 250, height: 60)
-                        .background(.white)
-                        .cornerRadius(10, antialiased: false)
+                    ButtonTextView()
                 }
                     
                 Spacer()
@@ -88,5 +69,50 @@ struct WeatherDayView: View {
                 .font(.system(size: 28, weight: .medium))
                 .foregroundColor(.white)
         }
+    }
+}
+
+struct BackgroundView: View {
+    var isNight:Bool
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]), startPoint: .topLeading, endPoint: .bottomTrailing  )
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityView: View {
+    var body: some View {
+        Text("Dhaka, BD")
+            .foregroundColor(.white)
+            .font(.title)
+            .fontWeight(.bold)
+            .padding()
+    }
+}
+
+struct MainWeatherView: View {
+    var body: some View {
+        VStack(spacing:10){
+            Image(systemName: "cloud.sun.fill")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            Text("76°")
+                .font(.system(size: 50, weight: .medium))
+                .foregroundColor(.white)
+        }.padding(.bottom,40)
+    }
+}
+
+struct ButtonTextView: View {
+    var body: some View {
+        Text("Change Day Time")
+            .foregroundColor(.blue)
+            .fontWeight(.bold)
+            .font(.title3)
+            .frame(width: 250, height: 60)
+            .background(.white)
+            .cornerRadius(10, antialiased: false)
     }
 }
